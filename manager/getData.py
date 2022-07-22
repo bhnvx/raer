7e98e83146983e5a8ecd1cae3ec86c6e34bfa853
KyoungHwan <xzt______@naver.com>
2022-07-22 16:30:19 +0900

    url link connect

--- manager/getData.py
+++ manager/getData.py
@@ -18,20 +18,25 @@ def events_of_get_contents(num: int) -> List[str]:
     url = _generate_url(num)
     page = _get_page(url)
     raw = page.find_all("span")
+    urls = page.find_all("a")[99:109]
 
     res = []
+    url = []
 
-    for r in raw:
+    for r in raw[18:28]:
         res.append(r.text)
 
-    return res[18:28]
+    for u in urls:
+        url.append("https://maplestory.nexon.com"+u.get('href'))
+
+    return res, url
 
 
 def create_events_dict(num: int) -> Dict:
     events = dict()
-    items = events_of_get_contents(num)
+    items, url = events_of_get_contents(num)
 
     for i in range(len(items)):
-        events[f"item{i}"] = items[i]
+        events[f"item{i}"] = {"notice": items[i], "url": url[i]}
 
     return events
